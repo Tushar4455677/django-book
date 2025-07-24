489716b6234b48dd1d89194824170e4e39af7e2c
Tushar Gupta <161271204+Tushar4455677@users.noreply.github.com>
2025-07-24 18:04:55 +0530

    Update settings.py

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -18,6 +18,8 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = os.environ.get("SECRET_KEY", "dummy-secret-for-dev")
+
 # settings.py
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
